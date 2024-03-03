bf4e68db77a33b0eb0df358b1141a32e2ca6e6a9
radulianoo <octav@raduliano.tech>
2024-03-03 15:30:22 +0200

    React props , propTypes and defaultProps added.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
 import Button from "./Button/Button";
+import Student from "./Student";
 
 function App() {
   return (
@@ -17,6 +18,11 @@ function App() {
       <Card />
       <Food />
       <Button />
+      <Student name="Spongebob" age={30} isStudent={false} />
+      <Student name="Tom" age={90} isStudent={true} />
+      <Student name="Jerry" age={90} isStudent={true} />
+      <Student name="Mickey" age={10} isStudent={false} />
+      <Student />
       <Footer />
     </>
   );
